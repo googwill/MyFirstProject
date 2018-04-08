6c4c06bf403ba426942c89c56f3f66e114bed998
Z1 <Z1@Z1-HP>
2018-04-07 23:02:41 -0400

    Initial checkin My First Project.

---
+++ My First Project/src/code/StartClass.java
@@ -0,0 +1,18 @@
+package code;
+
+import testPackage.TestClass;
+
+public class StartClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		TestClass myObject = new TestClass();
+		int num = myObject.GetNumber();
+		System.out.println(num);
+	
+		myObject.display();
+	}
+
+}
---
+++ My First Project/src/testPackage/TestClass.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+public class TestClass {	
+	int a = 123;    
+	public int GetNumber(){
+		int abc = 234;
+		int xyz = abc * abc;
+		return xyz;
+	}
+	
+	public void display() {
+		
+		System.out.print("test package!");
+	}
+	
+    
+}
+   
