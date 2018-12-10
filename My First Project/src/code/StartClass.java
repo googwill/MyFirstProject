e9b235acaacda230661158a008a7276caafeb228
Z1 <Z1@Z1-HP>
2018-12-09 19:41:41 -0500

    # WARNING: head commit changed in the meantime
    check in 12/9 for test
    
    Added ignore file

--- My First Project/src/code/StartClass.java
+++ My First Project/src/code/StartClass.java
@@ -7,13 +7,13 @@ public class StartClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		
+		   
 		TestClass myObject = new TestClass();
 		int num = myObject.GetNumber();
 		System.out.println(num);
 		
 		myObject.display();
-		System.out.println("test 1");
+		System.out.println("test 222");
 		
 	}
 
